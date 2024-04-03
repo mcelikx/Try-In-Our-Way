f34400f00807900fa6e3871023adf5c5adad1fa1
mcelikx <muhammedcelik.itu@gmail.com>
2024-04-03 23:59:54 +0300

    restaurant detail

--- app/restaurants/[restaurant]/page.tsx
+++ app/restaurants/[restaurant]/page.tsx
@@ -1,4 +1,19 @@
-const RestaurantPage = ({
+async function getData({ restaurantId }: { restaurantId: string }) {
+  const res = await fetch(
+    `${process.env.API_URL}/api/restaurants/${restaurantId}/?populate=*`
+  );
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const RestaurantPage = async ({
   params,
 }: {
   params: {
@@ -6,7 +21,9 @@ const RestaurantPage = ({
     slug: string;
   };
 }) => {
-  console.log(params.restaurant);
+  const data = await getData({ restaurantId: params.restaurant });
+
+  console.log(data);
   return (
     <div>
       <h1>{String(params.restaurant)}</h1>
