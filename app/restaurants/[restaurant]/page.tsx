5e1ef98411066920dcad69209169d4044811f21e
mcelikx <muhammedcelik.itu@gmail.com>
2024-05-08 23:00:05 +0300

    shadcn integrated

--- app/restaurants/[restaurant]/page.tsx
+++ app/restaurants/[restaurant]/page.tsx
@@ -1,6 +1,9 @@
 import Map from "@/app/_components/Map";
 import Image from "next/image";
 import styles from "./styles.module.css";
+import { Divider } from "@nextui-org/react";
+import Link from "next/link";
+import { Button } from "@nextui-org/react";
 async function getData({ restaurantId }: { restaurantId: string }) {
   const res = await fetch(
     `${process.env.API_URL}/api/restaurants?filters[url][$eq]=${restaurantId}&populate=*`
@@ -31,30 +34,54 @@ const RestaurantPage = async ({
   console.log(restaurantImage);
   const mapSrc = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
   &q=${restaurant.title},${restaurant.map.address}`;
-  // console.log(data);
+  console.log(restaurant);
   return (
     <div className={styles.root}>
-      <div className={styles.restaurantHead}>
-        <h1 className={styles.restaurantTitle}>{restaurant.title}</h1>
-        <div className={styles.restaurantImage}>
-          <Image
-            src={restaurantImage}
-            alt={restaurant.images.data[0].attributes.caption || ""}
-            width={300}
-            height={200}
-          />
+      <div className="bg-white shadow-md rounded-lg p-4 max-w-4xl mx-auto">
+        <div className="flex items-center justify-between mb-4">
+          <h1 className="text-2xl font-bold text-gray-800">
+            {restaurant.title}
+          </h1>
+          <div>
+            <button className="p-1 rounded-full text-gray-500 hover:text-gray-700">
+              <span className="sr-only">Save</span>
+              {/* Icon placeholder */}
+            </button>
+            <button className="p-1 rounded-full text-gray-500 hover:text-gray-700">
+              <span className="sr-only">Share</span>
+              {/* Icon placeholder */}
+            </button>
+          </div>
         </div>
-        <div className={styles.restaurantInfo}>
-          <p>
-            <span className={styles.restaurantInfoTitle}>Location:</span>{" "}
-            {restaurant.location}
-          </p>
-          <p>
-            <span className={styles.restaurantInfoTitle}>Cuisine:</span>{" "}
-            {restaurant.cuisine || "Not specified"}
-          </p>
+        <div className="flex items-center space-x-2 text-sm text-gray-600">
+          <div className="flex items-center">
+            <span className="text-green-500">★★★★★</span>
+            <span>({restaurant.reviewCount})</span>
+          </div>
+          {/* <span className="text-gray-400">|</span>
+          <span>#7 of 6,831 Restaurants in Singapore</span> */}
+        </div>
+        <div className="mt-2 text-sm text-gray-600">
+          <span>$$$$</span> · <span>{restaurant.cuisine}</span>
+        </div>
+        <div className="mt-2 text-sm text-gray-600">
+          <address>{restaurant.map.address}</address>
+        </div>
+        <div className="flex space-x-4 mt-2 text-sm">
+          <a
+            href={`tel:${restaurant.telephone}`}
+            className="text-blue-500 hover:underline"
+          >
+            {restaurant.telephone}
+          </a>
+          <Link target="_blank" href={`${restaurant.menuUrl}`}>
+            <p className="text-blue-500 hover:underline">Menu</p>
+          </Link>
+          <span className="text-gray-400">|</span>
+          <span>Closed now</span>
         </div>
       </div>
+
       <div className={styles.restaurantBody}>Restaurant Body</div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,76 +1,81 @@
 import type { Config } from "tailwindcss";
 import { nextui } from "@nextui-org/react";
 
-const defaultTheme = require("tailwindcss/defaultTheme");
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        // Primary palette
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
-          light: "#63b3ed",
-          DEFAULT: "#4299e1",
-          dark: "#3182ce",
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        // Secondary palette
         secondary: {
-          light: "#fbd38d",
-          DEFAULT: "#f6ad55",
-          dark: "#ed8936",
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
-        // Tertiary and additional palettes can be defined similarly
-        // Neutral palette
-        neutral: {
-          100: "#f7fafc",
-          200: "#edf2f7",
-          300: "#e2e8f0",
-          400: "#cbd5e0",
-          500: "#a0aec0",
-          600: "#718096",
-          700: "#4a5568",
-          800: "#2d3748",
-          900: "#1a202c",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        // UI colors
-        success: {
-          light: "#9ae6b4",
-          DEFAULT: "#48bb78",
-          dark: "#38a169",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
-        info: {
-          light: "#bee3f8",
-          DEFAULT: "#4299e1",
-          dark: "#3182ce",
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
-        warning: {
-          light: "#fbd38d",
-          DEFAULT: "#f6ad55",
-          dark: "#ed8936",
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
-        danger: {
-          light: "#feb2b2",
-          DEFAULT: "#fc8181",
-          dark: "#f56565",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
-        // Add more custom colors as needed
       },
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
-};
+  plugins: [nextui(), require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
